fc86be9321c66ce0a4c1bf62802b7216686eba67
Johannes Kiesel <Johannes.Kiesel@gesis.org>
2026-06-08 00:06:19 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 jQuery(document).ready(function( $ ) {
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
-
   // Header fixed on scroll
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
@@ -30,25 +17,6 @@ jQuery(document).ready(function( $ ) {
   if (window.matchMedia("(max-width: 767px)").matches) {
     $('#intro').css({ height: $(window).height() });
   }
-
-  // Initiate the wowjs animation library
-  new WOW().init();
-
-  // Initialize Venobox
-  $('.venobox').venobox({
-    bgcolor: '',
-    overlayColor: 'rgba(6, 12, 34, 0.85)',
-    closeBackground: '',
-    closeColor: '#fff'
-  });
-
-  // Initiate superfish on nav menu
-  $('.nav-menu').superfish({
-    animation: {
-      opacity: 'show'
-    },
-    speed: 400
-  });
 
   // Mobile Navigation
   if ($('#nav-menu-container').length) {
@@ -123,24 +91,6 @@ jQuery(document).ready(function( $ ) {
       }
     }
   });
-
-  // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center:true,
-    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
-    }
-  });
-
-  // Buy tickets select the ticket type on click
-  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var ticketType = button.data('ticket-type');
-    var modal = $(this);
-    modal.find('#ticket-type').val(ticketType);
-  })
 
 // custom code
 
